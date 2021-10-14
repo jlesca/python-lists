67df8909bed1630272fd879e49525f5b09cf20d6
José Lescano <92216756+jlesca@users.noreply.github.com>
2021-10-14 05:48:16 -0300

    Create creating-lists.py

---
+++ creating-lists.py
@@ -0,0 +1,23 @@
+# CREANDO LISTAS EN PYTHON
+# Usamos las listas para almacenar multiples datos en una sola variable.
+# Las listas son uno de los cuatros tipos de datos que nos permiten almacenar datos. 
+# Luego existen las TUPLAS, SETS, DICTIONARIES. Todas con difententes cualidades y funcionalidades.
+
+# Voy a crear una lista de colores
+# Los items de la lista deben estar entre [llaves], y separados por coma (,).
+# En ellas se pueden almacenar cualquier tipo de datos (int, float, strings, booleans, etc)
+
+colors = ['red', 'green', 'blue'] # Creo la variable y asigno sus items.
+print(colors) # Mostrará el contenido de mi variable.
+
+
+# Las Listas tienen características definidas:
+# Son Ordenadas, porque los items tienen un orden y una única posición que no cambia.
+# Son Modificables, porque se pueden alterar su contenido. Agregar, modificar o quitar items.
+# Permiten duplicar su contenido, es decir puede existir repetidas veces sus items.
+
+
+# Podemos saber la cantidad de items de una lista con la función LEN()
+
+print(len(colors)) # Mostrará la cantidad de items, en este casó será 3.
+
