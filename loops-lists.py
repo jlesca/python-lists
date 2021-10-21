6096b1fbed839c38a827d69f85acaa6f3efe278c
José Lescano <92216756+jlesca@users.noreply.github.com>
2021-10-21 08:46:38 -0300

    Create loops-lists.py

---
+++ loops-lists.py
@@ -0,0 +1,6 @@
+# LOOPS CON NUESTRAS LISTAS
+# Podemos crear un bucle de funciones con el contenido de nuestra lista.
+
+colors=['red', 'green', 'blue'] # Creo la variable y asigno sus elementos.
+for item in colors: # Asigno una variable para cada elemento llamada 'item'
+  print(item) # Muestro cada elemento.
